d76a962ab0391af39fe03a49e879fc5b06e7c2f0
Viktor Somodi <somodi.viktor@gmail.com>
2015-08-28 07:53:11 +0200

    add dummy tests

---
+++ test/directive.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Directive', function() {
+
+  it('should work', function() {
+    this.expect(1).to.eql(1);
+  });
+
+});
---
+++ test/service.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Service', function() {
+
+  it('should work', function() {
+    this.expect(1).to.eql(1);
+  });
+
+});
